916c7a2e7a7f037f095f859ab426feceadcbda26
Fidelio Ciandy <fiocheah08@gmail.com>
2023-11-15 17:13:27 -0600

    Create blockchain-peer.py

---
+++ blockchain-peer.py
@@ -0,0 +1,42 @@
+import socket
+import json
+import uuid
+
+# TODO:
+# 1. GOSSIP
+# 2. Consensus
+# 3. Create Chain
+# 4. Add Block
+
+SILICON_HOST, SILICON_PORT = "192.168.0.27", 8999
+
+
+def announce_gossip(gossip_conn):
+    gossip_message = {
+        "type": "GOSSIP",
+        "host": "192.168.0.27",
+        "port": 8999,
+        "id": str(uuid.uuid4()),
+        "name": "Hello World!",
+    }
+    gossip_conn.send(json.dumps(gossip_message).encode())
+    return gossip_conn.recv(4096)
+
+
+def gossip(host, port):
+    # TODO:
+    # 1. connect to one well-known host
+    gossip_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    gossip_conn.connect((host, port))
+    print(announce_gossip(gossip_conn))
+    # 2. Reply gossip message received
+    print()
+
+
+def main():
+    gossip(SILICON_HOST, SILICON_PORT)
+    print("TODO here")
+
+
+if __name__ == "__main__":
+    main()
